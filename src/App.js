a27a9c2d1fa835d163f7c452635685c57e8f5c71
v.anhad-at-632228229419 <vanhad72@gmail.com>
2023-10-04 20:58:04 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { mean, median, mode } from './Utils';
+
+// Sample dataset (replace this with your dataset)
+const data = [
+    { Alcohol: 1, Flavanoids: 5, Ash: 2, Hue: 3, Magnesium: 4 },
+    { Alcohol: 15, Flavanoids: 55, Ash: 52, Hue: 53, Magnesium: 45 },
+    // ... add more data points
+];
+
+function calculateStats(attribute) {
+    // Group by Alcohol class
+    const grouped = data.reduce((acc, curr) => {
+        (acc[curr.Alcohol] = acc[curr.Alcohol] || []).push(curr);
+        return acc;
+    }, {});
+
+    // Compute stats for each class
+    const stats = {};
+    for (let cls in grouped) {
+        const values = grouped[cls].map(item => item[attribute]);
+        stats[cls] = {
+            mean: mean(values),
+            median: median(values),
+            mode: mode(values)
+        };
+    }
+
+    return stats;
+}
+
+const FlavanoidsStats = calculateStats('Flavanoids');
+
+// Add Gamma to each data point
+data.forEach(point => point.Gamma = (point.Ash * point.Hue) / point.Magnesium);
+
+const GammaStats = calculateStats('Gamma');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Measure</th>
+                        {Object.keys(FlavanoidsStats).map(cls => <th key={cls}>Class {cls}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Flavanoids Mean</td>
+                        {Object.values(FlavanoidsStats).map((stat, idx) => <td key={idx}>{stat.mean}</td>)}
+                    </tr>
+                    <tr>
+                        <td>Flavanoids Median</td>
+                        {Object.values(FlavanoidsStats).map((stat, idx) => <td key={idx}>{stat.median}</td>)}
+                    </tr>
+                    <tr>
+                        <td>Flavanoids Mode</td>
+                        {Object.values(FlavanoidsStats).map((stat, idx) => <td key={idx}>{stat.mode}</td>)}
+                    </tr>
+                    <tr>
+                        <td>Gamma Mean</td>
+                        {Object.values(GammaStats).map((stat, idx) => <td key={idx}>{stat.mean}</td>)}
+                    </tr>
+                    <tr>
+                        <td>Gamma Median</td>
+                        {Object.values(GammaStats).map((stat, idx) => <td key={idx}>{stat.median}</td>)}
+                    </tr>
+                    <tr>
+                        <td>Gamma Mode</td>
+                        {Object.values(GammaStats).map((stat, idx) => <td key={idx}>{stat.mode}</td>)}
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Utils/index.js
@@ -0,0 +1,32 @@
+// utils.js
+function mean(arr) {
+    const sum = arr.reduce((acc, val) => acc + val, 0);
+    return +(sum / arr.length).toFixed(3);
+}
+
+function median(arr) {
+    const sorted = [...arr].sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    if (sorted.length % 2 === 0) {
+        return +((sorted[mid - 1] + sorted[mid]) / 2).toFixed(3);
+    }
+    return +sorted[mid].toFixed(3);
+}
+
+function mode(arr) {
+    const freq = {};
+    let maxFreq = 0;
+    let modeVal;
+
+    for (let num of arr) {
+        freq[num] = (freq[num] || 0) + 1;
+        if (freq[num] > maxFreq) {
+            maxFreq = freq[num];
+            modeVal = num;
+        }
+    }
+
+    return +modeVal.toFixed(3);
+}
+
+export { mean, median, mode };
